2dbe0e087176e51fd6461c103335f0b481b948bf
Auto Logon <autologon@CSED.LAB>
2022-10-19 19:34:54 -0300

    exercicio1 feito

---
+++ App.java
@@ -0,0 +1,37 @@
+package POO;
+import java.util.Scanner;
+
+class App{
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        double valorProduto, soma, aumento;
+        int cod;
+        aumento = 0;
+
+        Scanner n = new Scanner(System.in);
+
+        System.out.println("Qual o valor do produto");
+        valorProduto = n.nextDouble();
+
+        System.out.println("Qual o código do aumento");
+        cod = n.nextInt();
+
+        if(cod == 1){
+            aumento = 0.10;
+        }else if(cod == 2){
+            aumento = 0.15;
+        }else if(cod == 3){
+            aumento = 0.20;
+        }else if(cod == 4){
+            aumento = 0.25;
+        }
+
+        soma = valorProduto * aumento;
+
+        System.out.println("O novo valor do produto é " + soma);
+
+        n.close();
+    }
+}
